f937abe364228408cbd9cc113a59de491a9771ac
dcaisse1337-web <dcaisse1337@gmail.com>
2026-02-08 04:29:49 +0100

    Add initial index.php for Cyber Flash OS

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cyber Flash OS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="bg-speed"></div>
+
+<div class="container">
+    <h1 class="title">⚡ CYBER FLASH SEARCH ⚡</h1>
+
+    <form action="search.php" method="GET" class="search-box">
+        <input type="text" name="q" placeholder="Search target..." required>
+        <button>SCAN</button>
+    </form>
+</div>
+
+<script src="script.js"></script>
+</body>
+</html>
